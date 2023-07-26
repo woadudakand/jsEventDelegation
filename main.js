11c87346941ca7dbd2b30e14a9b5835e030f9e2b
Woadud Akand <49744359+woadudakand@users.noreply.github.com>
2023-07-27 00:34:48 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-function eventDelegation(event, selector, program) {
-    document.body.addEventListener(event, function(e) {
+function eventDelegation(event, selector, callBack) {
+    document.body.addEventListener(event, function(e) {               
             document.querySelectorAll(selector).forEach(elem => {
-                if (e.target === elem) {
-                        program(e);
+                if (e.target === elem || e.target.closest(selector) === elem) {
+                        callBack(e, document.querySelector(selector));
                 }
             })
     });
